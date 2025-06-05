19eede6dc9ec9104eacb9c345b7fc3730ff91e45
ErbetoMiheretab <mengeshamiheretab36@gmail.com>
2025-06-05 13:23:06 +0300

    Added Task model and serializer

---
+++ taskmanager/task_api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Task
+
+
+
+class TaskSer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'completed', 'created_at']
+
